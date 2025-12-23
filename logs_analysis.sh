4e8d387ec78925cd7ef528c790375571d09dc1e4
Mansoor <mdmansooralam90@gmail.com>
2025-12-23 17:53:01 +0000

    Updated the script with the absolute path of the log files

--- logs_analysis.sh
+++ logs_analysis.sh
@@ -7,78 +7,78 @@ echo -e "\n***********************Starting Logs Analysis***********************"
 #Determine the files that were updated in the last 24 hours 
 echo -e "\nBelow files were updated in the last 24 hours"
 echo -e "\n"
-find . -name "*.log" -mtime -1
+find /mnt/c/Users/MASROOR/Desktop/logs_ingestion -name "*.log" -mtime -1 #Using the absolute path of the file to ensure the execution irrespective of the script file location
 
 echo -e "\n=====================Analysing Application.log file====================="
 echo -e "\n"
 
-grep "WARNING" application.log
+grep "WARNING" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log
 
-echo -e "\nNumber of logs of WARNING type is: " $(grep -c "WARNING" application.log) #Command Substituion
-
-echo -e "\n---------------------------------------------------------------"
-echo -e "\n"
-
-grep "EXCEPTION" application.log
-
-echo -e "\nNumber of logs of EXCEPTION type is: " $(grep -c "EXCEPTION" application.log) #Command Substituion
+echo -e "\nNumber of logs of WARNING type is: " $(grep -c "WARNING" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log) #Command Substituion
 
 echo -e "\n---------------------------------------------------------------"
 echo -e "\n"
 
-grep "ERROR" application.log
+grep "EXCEPTION" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log
 
-echo -e "\nNumber of logs of ERROR type is: " $(grep -c "ERROR" application.log) #Command Substituion
-
-echo -e "\n---------------------------------------------------------------"
-echo -e "\n"
-
-grep "CRITICAL" application.log
-
-echo -e "\nNumber of logs of CRITICAL type is: " $(grep -c "CRITICAL" application.log) #Command Substituion
+echo -e "\nNumber of logs of EXCEPTION type is: " $(grep -c "EXCEPTION" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log) #Command Substituion
 
 echo -e "\n---------------------------------------------------------------"
 echo -e "\n"
 
-grep "FATAL" application.log
+grep "ERROR" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log
 
-echo -e "\nNumber of logs of FATAL type is: " $(grep -c "FATAL" application.log) #Command Substituion
+echo -e "\nNumber of logs of ERROR type is: " $(grep -c "ERROR" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log) #Command Substituion
+
+echo -e "\n---------------------------------------------------------------"
+echo -e "\n"
+
+grep "CRITICAL" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log
+
+echo -e "\nNumber of logs of CRITICAL type is: " $(grep -c "CRITICAL" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log) #Command Substituion
+
+echo -e "\n---------------------------------------------------------------"
+echo -e "\n"
+
+grep "FATAL" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log
+
+echo -e "\nNumber of logs of FATAL type is: " $(grep -c "FATAL" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/application.log) #Command Substituion
 
 echo -e "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 
 echo -e "\n=====================Analysing System.log file====================="
 echo -e "\n"
 
-grep "WARNING" system.log
+grep "WARNING" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log
 
-echo -e "\nNumber of logs of WARNING type is: " $(grep -c "WARNING" system.log) #Command Substituion
-
-echo -e "\n---------------------------------------------------------------"
-echo -e "\n"
-
-grep "EXCEPTION" system.log
-
-echo -e "\nNumber of logs of EXCEPTION type is: " $(grep -c "EXCEPTION" system.log) #Command Substituion
+echo -e "\nNumber of logs of WARNING type is: " $(grep -c "WARNING" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log) #Command Substituion
 
 echo -e "\n---------------------------------------------------------------"
 echo -e "\n"
 
-grep "ERROR" system.log
+grep "EXCEPTION" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log
 
-echo -e "\nNumber of logs of ERROR type is: " $(grep -c "ERROR" system.log) #Command Substituion
-
-echo -e "\n---------------------------------------------------------------"
-echo -e "\n"
-
-grep "CRITICAL" system.log
-
-echo -e "\nNumber of logs of CRITICAL type is: " $(grep -c "CRITICAL" system.log) #Command Substituion
+echo -e "\nNumber of logs of EXCEPTION type is: " $(grep -c "EXCEPTION" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log) #Command Substituion
 
 echo -e "\n---------------------------------------------------------------"
 echo -e "\n"
 
-grep "FATAL" system.log
+grep "ERROR" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log
 
-echo -e "\nNumber of logs of FATAL type is: " $(grep -c "FATAL" system.log) #Command Substituion
+echo -e "\nNumber of logs of ERROR type is: " $(grep -c "ERROR" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log) #Command Substituion
+
+echo -e "\n---------------------------------------------------------------"
+echo -e "\n"
+
+grep "CRITICAL" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log
+
+echo -e "\nNumber of logs of CRITICAL type is: " $(grep -c "CRITICAL" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log) #Command Substituion
+
+echo -e "\n---------------------------------------------------------------"
+echo -e "\n"
+
+grep "FATAL" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log
+
+echo -e "\nNumber of logs of FATAL type is: " $(grep -c "FATAL" /mnt/c/Users/MASROOR/Desktop/logs_ingestion/system.log) #Command Substituion
 
 echo -e "\nANALYSIS COMPLETED!"
